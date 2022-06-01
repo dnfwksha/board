2dc2cd1c230cd9c2bd4b0c6d03dc80cb6da1252d
dnfwk <dnfwksha@naver.com>
2022-06-01 20:44:41 +0900

    delete

--- src/main/java/com/study/board/controller/BoardController.java
+++ src/main/java/com/study/board/controller/BoardController.java
@@ -46,4 +46,12 @@ public class BoardController {
 
         return "boardview";
     }
+
+    @GetMapping("/board/delete")
+    public String boardDelete(Integer id){
+
+        boardService.boardDelete(id);
+
+        return "redirect:/board/list";
+    }
 }
